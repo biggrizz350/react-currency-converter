066c96be8aed68754b901c902a0c85e08ea6c268
Robert Witters <41020734+biggrizz350@users.noreply.github.com>
2021-11-18 17:04:59 -0500

    Drop down menus integrated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import CurrencyRow from "./CurrencyRow";
 
+const BASE_URL =
+  "http://api.exchangeratesapi.io/v1/latest?access_key=6bfe8baa55bc66828b0b229fc5c13cd1";
+
 function App() {
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  console.log(currencyOptions);
+
+  useEffect(() => {
+    fetch(BASE_URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)]) //we destructure the array, the curriences are  
+      });                                                           //set to different options
+  }, []);
+
   return (
     <>
       <h1>Convert</h1>
-      <CurrencyRow />
+      <CurrencyRow 
+      currencyOptions={currencyOptions}/>
       <div className="equals">=</div>
-      <CurrencyRow />
+      <CurrencyRow 
+      currencyOptions={currencyOptions}/>
     </>
   );
 }
--- src/CurrencyRow.js
+++ src/CurrencyRow.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
-export default function CurrencyRow() {
+export default function CurrencyRow(props) {
+    const {
+        currencyOptions
+    } = props   //Destructure the props into the currencyOptions
+
     return (
         <div>
             <input type="number" className="input"/>
             <select>
-                <option value="Hi">Hi</option>
+                {currencyOptions.map(option => (
+                    <option key={option} value={option}>{option}</option>
+                ))} {/*Translates array of currency options into actual options*/}
+    
             </select>
         </div>
     )
